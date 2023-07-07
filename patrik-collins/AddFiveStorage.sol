e717034a1d67240d674f330d4137086b3c739c37
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-07 15:00:04 +0530

    inheritance

--- patrik-collins/AddFiveStorage.sol
+++ patrik-collins/AddFiveStorage.sol
@@ -1,2 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10; //stating our version
+
+import "./SimpleStorage.sol";
+
+contract AddFiveStorage is SimpleStorage {
+    function sayHello() public pure returns( string memory){
+        return "Hello";
+    }
+
+    // overiders
+    // virtual override
+
+    // function store( uint256, _newNumber) public {
+    // }
+
+}
