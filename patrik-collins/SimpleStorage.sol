40773d8b0d81400cb821b1e3cf0f7cc7575de9cb
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-07 13:51:19 +0530

    importing contracts

--- patrik-collins/SimpleStorage.sol
+++ patrik-collins/SimpleStorage.sol
@@ -2,31 +2,27 @@
 pragma solidity ^0.8.10; //stating our version
 
 contract SimpleStorage {
-    // basic types: boolean, unit, int, address, bytes
-    uint256 public myfavouriteNumber = 5; //default value 0
+    uint256 myFavoriteNumber;
 
-    // uint256[] listofFavouriteNumbers; //[ 0, 78, 90]
-    struct Person{
-        uint256 favouriteNumber;
+    struct Person {
+        uint256 favoriteNumber;
         string name;
     }
-
-    // Person public pat = Person(7,"Pat");
+    // uint256[] public anArray;
     Person[] public listOfPeople;
 
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-
-    function store( uint256 _favouriteNumber) public {
-        myfavouriteNumber = _favouriteNumber;
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
     }
 
-    function retrieve() public view returns(uint256){
-        return myfavouriteNumber;
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 
-    function addPerson (string memory _name, uint256 _favouriteNumber) public {
-        listOfPeople.push(Person(_favouriteNumber, _name));
- 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
 }
--- patrik-collins/StorageFactory.sol
+++ patrik-collins/StorageFactory.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10; //stating our version
+pragma solidity ^0.8.18; //stating our version
+import "./SimpleStorage.sol";
+
+contract StorageFactory{
+    
+    // uint256 public favouriteNumber
+    // type visibility 
+
+    SimpleStorage public simpleStorage;
+
+    function createSimpleStorageContract() public {
+            simpleStorage = new SimpleStorage();
+    }
+}
