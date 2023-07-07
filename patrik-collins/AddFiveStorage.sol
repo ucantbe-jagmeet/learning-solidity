64ed88bfa5fc1a84c5a5b8d32cc278bb951f2710
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-07 15:02:26 +0530

    overriding function

--- patrik-collins/AddFiveStorage.sol
+++ patrik-collins/AddFiveStorage.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.10; //stating our version
 import "./SimpleStorage.sol";
 
 contract AddFiveStorage is SimpleStorage {
-    function sayHello() public pure returns( string memory){
-        return "Hello";
-    }
+    // function sayHello() public pure returns( string memory){
+    //     return "Hello";
+    // }
 
     // overiders
     // virtual override
 
-    // function store( uint256, _newNumber) public {
-    // }
+    function store( uint256 _newNumber) public override  {
+        myFavoriteNumber = _newNumber + 5;
+    }
 
 }
