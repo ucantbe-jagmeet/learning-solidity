b7fbc8f37b83e1f0d10472bcfef8b1f8b93567e5
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-11 03:53:27 +0530

    contract balance update

---
+++ code-eater/21-contract-bal-update/contract-update.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10; //stating our version
+
+
+contract pay {
+
+    address payable user = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+
+    function payEther() public payable {
+        
+    }
+    function getBalance() public view returns (uint){
+        return address(this).balance;
+    }
+    function sendEtherAccount() public {
+       user.transfer( 1 ether);
+    }
+}
