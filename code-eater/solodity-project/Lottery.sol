0c1a5506f36fd873c46e0785b6d6169529710f13
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-12 01:07:31 +0530

    random function

--- code-eater/solodity-project/Lottery.sol
+++ code-eater/solodity-project/Lottery.sol
@@ -19,4 +19,7 @@ contract Lottery{
         return  address(this).balance;       
     }
 
+    function random() public view returns (uint){
+      return uint ( keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants.length)));
+    }
 }
