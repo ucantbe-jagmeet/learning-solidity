88cbfc9fe437198b13894d0c34a284dc43b195e6
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-13 00:43:46 +0530

    setting up struct and mapping

--- code-eater/event-organisation/Event.sol
+++ code-eater/event-organisation/Event.sol
@@ -2,5 +2,16 @@
  pragma solidity >=0.5.0 <0.9.0;
 
  contract EventContract{
-     
+     struct Event{
+         address organizer;
+         string name;
+         uint date;
+         uint price;
+         uint ticketCount;
+         uint ticketRemain;
+     }
+
+     mapping( uint => Event) public events;
+     mapping( address => mapping(uint=>uint)) public tickets; // holds tickets
+     uint public nextId;
  }
