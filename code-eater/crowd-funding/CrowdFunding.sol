497e42c6b046ca9cd81582fcffc78fc40762eda2
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-14 03:22:15 +0530

    voteRequest function

--- code-eater/crowd-funding/CrowdFunding.sol
+++ code-eater/crowd-funding/CrowdFunding.sol
@@ -66,4 +66,12 @@
          newRequest.completed = false;
          newRequest.noOfVoters = 0;
      }
+
+     function voteRequest( uint _requestNo) public{
+         require(contributors[msg.sender] > 0, "You must be a contributor");
+         Request storage thisRequest = requests[_requestNo];
+         require(thisRequest.voters[msg.sender] == false, "You already voted");
+         thisRequest.voters[msg.sender] = true;
+         thisRequest.noOfVoters++;
+     }
  }
