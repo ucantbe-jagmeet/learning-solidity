5da2a26d3c05472e0a22703d0c33bf6703dbede0
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-14 00:59:02 +0530

    setting up constructor and variable

--- code-eater/crowd-funding/CrowdFunding.sol
+++ code-eater/crowd-funding/CrowdFunding.sol
@@ -2,5 +2,18 @@
  pragma solidity >=0.5.0 <0.9.0;
 
  contract CrowdFunding{
-     
+     mapping( address => uint) public contributors;
+     address public manager;
+     uint public minimumContribution;
+     uint public deadline;
+     uint public target;
+     uint public raisedAmount;
+     uint public noOfContributors;
+
+     constructor(uint _target, uint _deadline) {
+         target = _target;
+         deadline = block.timestamp + _deadline;
+         minimumContribution = 100 wei;
+         manager = msg.sender;
+     }
  }
