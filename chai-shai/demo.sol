ad83d90024b72c3c27719b90d0f8dd52c2e36900
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-07-21 16:40:44 +0530

    buyChai function

--- chai-shai/demo.sol
+++ chai-shai/demo.sol
@@ -2,5 +2,23 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract chai{
-    
+    struct Memo{
+        string name;
+        string message;
+        uint timestamp;
+        address from;
+    }
+
+    Memo[] memos;
+    address payable owner;
+
+    constructor (){
+        owner = payable(msg.sender);
+    }
+
+    function buyChai(string memory name , string memory message) public payable{
+        require( msg.value > 0 , "Please pay greater than 0 ether");
+        owner.transfer(msg.value);
+        memos.push( Memo(name, message, block.timestamp, msg.sender));
+    }
 }
