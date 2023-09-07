b2abfd244aa7dda836a565500abbb112ab021731
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-09-07 20:27:40 +0530

    state variable

---
+++ code-eater-2/02-state-variable/state.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 < 0.9.0;
+
+
+contract State {
+    uint public age;
+    uint public number;
+    // uint public age = 10;
+    
+    // constructor()  {
+    //     age = 10;
+    // }
+
+    // function setAge() public {
+    //     age = 10;
+    // }
+
+
+
+}
