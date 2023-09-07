bc9fc04568fb75081cbf55d87e550326837aa98c
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-09-07 20:52:22 +0530

    local variable

---
+++ code-eater-2/03-local-variable/main.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 < 0.9.0;
+
+
+contract Local {
+    function store() pure public returns (uint) {
+        // string memory name = "ucantbe";
+        uint age = 10;
+        return age;
+    }
+}
