5d05cb3883f08531d62db78a2481bda7ea14503c
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-09-27 21:10:49 +0530

    demo

--- by-stepher-grinder/solidity/01-intro/main.sol
+++ by-stepher-grinder/solidity/01-intro/main.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract demo {
+    string public  message;
+
+    function Inbox(string memory initialMessage) public {
+        message = initialMessage;
+    }
     
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
 }
