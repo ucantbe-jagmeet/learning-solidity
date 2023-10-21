34ac4997c9217ed0b669709b573aa57ff39fd386
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-21 21:29:56 +0530

    state variables

--- by-daulat-hussain/solidity/05-variables/demo.sol
+++ by-daulat-hussain/solidity/05-variables/demo.sol
@@ -5,12 +5,20 @@ contract demo {
     string public myState;
     uint public myNum;
 
-    string public defaultText="hey Jagmeet";
-    uint public defaultNum = 6;
+    // string public defaultText="hey Jagmeet";
+    // uint public defaultNum = 6;
 
-    bytes public defaultBytes = "Hey Jagmeet";
-    bytes public defaultBytesNo;
+    // bytes public defaultBytes = "Hey Jagmeet";
+    // bytes public defaultBytesNo;
 
+    constructor( string memory _text, uint _no){
+        myState = _text;
+        myNum = _no;
+    }
 
+    function update(string memory _text, uint _no) public {
+        myState = _text;
+        myNum = _no;
+    }
 
 }
