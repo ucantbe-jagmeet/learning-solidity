7c5ebe3af890bf364a5e31052ba2f7f109a0bf1d
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-21 21:28:05 +0530

    variables

---
+++ by-daulat-hussain/solidity/05-variables/demo.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract demo {
+    string public myState;
+    uint public myNum;
+
+    string public defaultText="hey Jagmeet";
+    uint public defaultNum = 6;
+
+    bytes public defaultBytes = "Hey Jagmeet";
+    bytes public defaultBytesNo;
+
+
+
+}
