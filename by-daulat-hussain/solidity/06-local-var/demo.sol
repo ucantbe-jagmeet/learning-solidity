ba50d9a13c5596830b894c3a9445d7c95ae90373
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-23 19:57:50 +0530

    global variable

--- by-daulat-hussain/solidity/06-local-var/demo.sol
+++ by-daulat-hussain/solidity/06-local-var/demo.sol
@@ -2,16 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract demo {
-    uint public myNumber;
+    // uint public myNumber;
 
-    function local() public returns (address, uint, uint) {
-        // variable define inside the function store
-        uint i = 345;
-        myNumber = i;
+    // function local() public returns (address, uint, uint) {
+    //     // variable define inside the function store
+    //     uint i = 345;
+    //     myNumber = i;
 
-        i+= 45;
+    //     i+= 45;
 
-        address myAddress = address(1);
-        return ( myAddress, myNumber, i);
+    //     address myAddress = address(1);
+    //     return ( myAddress, myNumber, i);
+    // }
+
+    address public owner;
+    address public number;
+    address public gaslimit;
+    address public nowOn;
+    address public value;
+    address public myblockhash;
+    address public difficulty;
+
+    constructor() {
+        owner = msg.sender;
+        myblockhash = block.coinbase;
+        myblockhash = block.coinbase;
+        // number = block.number;
     }
 }
