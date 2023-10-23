86b64d461c2ad21cba71441c5a7f47b07f8c514a
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-23 19:49:01 +0530

    local variable

--- by-daulat-hussain/solidity/06-local-var/demo.sol
+++ by-daulat-hussain/solidity/06-local-var/demo.sol
@@ -4,10 +4,14 @@ pragma solidity ^0.8.0;
 contract demo {
     uint public myNumber;
 
-    function local() public returns (uint) {
+    function local() public returns (address, uint, uint) {
         // variable define inside the function store
         uint i = 345;
         myNumber = i;
-        return myNumber;
+
+        i+= 45;
+
+        address myAddress = address(1);
+        return ( myAddress, myNumber, i);
     }
 }
