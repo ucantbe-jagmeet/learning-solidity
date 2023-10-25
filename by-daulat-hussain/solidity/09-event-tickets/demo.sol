af226afeb25ac539d5b8b3afa68c58a3c89ba9b1
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-25 19:08:08 +0530

    buy ticket function with contructor

--- by-daulat-hussain/solidity/09-event-tickets/demo.sol
+++ by-daulat-hussain/solidity/09-event-tickets/demo.sol
@@ -4,10 +4,25 @@ pragma solidity ^0.8.0;
 contract demo {
     uint public numberOfTicket;
     uint public ticketPrice;
+    uint public ticketId;
     uint public totalAmount;
     uint public startAt;
     uint public endAt;
-    uint public timestamp;
+    uint public timeRange;
     string public message= "Buy your first Event Ticket";
+
+    constructor(uint _ticketPrice) {
+        ticketPrice = _ticketPrice;
+        startAt = block.timestamp;
+        endAt= block.timestamp + 7 days;
+        timeRange = (endAt - startAt) / 60 / 60 / 24; // converting to seconds
+    }
+
+    function buyTicket(uint _value) public returns(uint) {
+        numberOfTicket++;
+        totalAmount += _value;
+        ticketId = numberOfTicket;
+        return ticketId;
+    }
 
 }
