ccba9e95c709da58e8da9d8ca4f1a0507c40bcc8
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-25 19:01:42 +0530

    setting up variables

---
+++ by-daulat-hussain/solidity/09-event-tickets/demo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract demo {
+    uint public numberOfTicket;
+    uint public ticketPrice;
+    uint public totalAmount;
+    uint public startAt;
+    uint public endAt;
+    uint public timestamp;
+    string public message= "Buy your first Event Ticket";
+
+}
