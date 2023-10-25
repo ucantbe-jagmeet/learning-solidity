37049d873c5ba89dd367025325732dff0b2a3afd
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-10-25 21:27:42 +0530

    constant keyword

--- by-daulat-hussain/solidity/10-constant/demo.sol
+++ by-daulat-hussain/solidity/10-constant/demo.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.0;
 
 contract Demo {
     // address public myAdd =0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 ;
-    address public constant MY_ADD =0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 ;
+    address constant MY_ADD =0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 ;
 
     // constant saves gas 
     // keyword given to that variable that doesnot change
 
-    function getConstant() public view returns (address) {
+    function getConstant() public view returns(address) {
         return MY_ADD;
     } 
 }
