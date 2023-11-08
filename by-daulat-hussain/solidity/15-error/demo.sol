b89ade2e35acd2944ef693d4dbe2958d5a4d17da
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-08 23:19:25 +0530

    view to pure

--- by-daulat-hussain/solidity/15-error/demo.sol
+++ by-daulat-hussain/solidity/15-error/demo.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract demo {
-    function checkInput(uint _input) public view returns(string memory) {
+contract Demo {
+    function checkInput(uint _input) public pure returns(string memory) {
         require(_input >=0, "invalid uint");
         require(_input <=255, "invalid uint");
 
         return "Input is uint8";
     }
 
-    function Odd(uint _input) public view returns (bool){
-        require(_input %2 !=0);
+    function Odd(uint _input) public pure returns (bool){
+        require(_input %2 !=0, "Not odd");
         return true;
     }
-    function Even(uint _input) public view returns (bool){
-        require(_input %2 ==0);
+    function Even(uint _input) public pure returns (bool){
+        require(_input %2 ==0, "Not Even");
         return true;
     }
 }
