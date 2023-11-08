70d456ab1fa23c22af76f5684193afed5698cd34
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-08 23:34:09 +0530

    removing artifacts

--- by-daulat-hussain/solidity/17-revert-error/demo.sol
+++ by-daulat-hussain/solidity/17-revert-error/demo.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract Demo {
+
+    function checkOverFlow(uint _num1, uint _num2) public pure returns(string memory, uint) {
+        
+        uint sum = _num1 + _num2;
+        if(sum<0 || sum > 255){
+            revert('Overflow exists');
+        } else{
+            return ('No OverFlow', sum);
+        }
+
+    }
     
 }
