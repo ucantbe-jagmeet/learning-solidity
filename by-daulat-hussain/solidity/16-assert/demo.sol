9530b5443508023bbef98581c922630209a6ad11
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-08 23:26:56 +0530

    get result function

--- by-daulat-hussain/solidity/16-assert/demo.sol
+++ by-daulat-hussain/solidity/16-assert/demo.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract Demo {
-    bool result;
+    bool public result;
 
     function checkOverFlow(uint _num1, uint _num2) public {
         uint sum = _num1 + _num2;
         assert( sum <= 255);
         result = true;
+    }
+    function getResult() public view returns (string memory) {
+        if(result == true){
+            return 'no  overflow';
+        } else {
+            return 'overflow exist';
+        }
     }
 }
