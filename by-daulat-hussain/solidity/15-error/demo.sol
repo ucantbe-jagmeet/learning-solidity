ed51b3dc789221c107c23424f10f889596fc2775
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-08 23:09:24 +0530

    require statement

--- by-daulat-hussain/solidity/15-error/demo.sol
+++ by-daulat-hussain/solidity/15-error/demo.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.0;
 
 contract demo {
     function checkInput(uint _input) public view returns(string memory) {
-        
+        require(_input >=0, "invalid uint");
+        require(_input <=255, "invalid uint");
+
+        return "Input is uint";
     }
 }
