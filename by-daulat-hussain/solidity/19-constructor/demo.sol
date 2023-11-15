d238b81318f4f09651e31815e8f5704d6b806a7e
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-15 19:28:01 +0530

    another ways to declare inheritance and its order

--- by-daulat-hussain/solidity/19-constructor/demo.sol
+++ by-daulat-hussain/solidity/19-constructor/demo.sol
@@ -23,3 +23,30 @@ contract B is X("Input to X"), Y("input to Y"){}
 contract C is X, Y{
     constructor(string memory _name, string memory _text) X(_name) Y(_text)  {}
 }
+
+
+// Parent constructores are always called in the order of inheritance
+// Regardless of the order of parent contracts listed in the constructor of the child contract
+
+// Order of constructor called:
+// -> 1. X 
+// -> 2. Y 
+// -> 3. B
+// -> 4. C
+
+contract D is X, Y{
+    constructor() X("X was called") Y("Y was called")  {}
+}
+// Order of constructor called:
+// -> 1. X 
+// -> 2. Y 
+// -> 3. D
+
+
+contract E is X, Y{
+    constructor() Y("Y was called") X("X was called")  {}
+}
+// Order of constructor called:
+// -> 1. X
+// -> 2. Y 
+// -> 3. E
