6d1e7b4e2e66cf8696127ccef91eb2b73944c722
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-15 18:31:52 +0530

    creating modifiers

--- by-daulat-hussain/solidity/18-modifier/demo.sol
+++ by-daulat-hussain/solidity/18-modifier/demo.sol
@@ -7,5 +7,22 @@ pragma solidity ^0.8.0;
 // ------> Restrict access. validate inputs, Guard against reetrancy hack
 
 contract Demo {
-    
+    address public owner;
+    uint public x = 10;
+    bool public locked;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,'Not owner');
+        _;
+    }
+
+    modifier validateData(address _addr){
+        require(_addr != address(0),'Not valid address');
+        _;
+    }
+
 }
