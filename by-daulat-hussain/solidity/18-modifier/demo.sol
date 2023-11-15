8115127f2b33ada40d84783025323c64db4bca02
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-11-15 18:27:24 +0530

    Important abour modifiers

---
+++ by-daulat-hussain/solidity/18-modifier/demo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Important take aways
+// -> Modifiers are code that can be run before and after the function calls
+// -> generally userd for three reasons
+// ------> Restrict access. validate inputs, Guard against reetrancy hack
+
+contract Demo {
+    
+}
