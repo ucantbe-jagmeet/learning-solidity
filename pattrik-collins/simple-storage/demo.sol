b83b520ffc829d17e6ef2b44230861a9cd48f153
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-12-13 18:51:14 +0530

    functions

--- pattrik-collins/simple-storage/demo.sol
+++ pattrik-collins/simple-storage/demo.sol
@@ -2,16 +2,26 @@
 pragma solidity ^0.8.0; // solidity versions
 
 contract Demo {
-    bool hasFavoriteNumber = true;
+    // bool hasFavoriteNumber = true;
 
-    uint favoriteNumber = 88; // int can be positive or negative , but uint is always positive
+    uint public favoriteNumber = 88; // int can be positive or negative , but uint is always positive
 
-    string favoriteNumberText = "88/ eighty-eight";
+    // string favoriteNumberText = "88/ eighty-eight";
 
-    address myAddress = 0x4a909F94bb4fb71b9E9dA8F3aAc58565310bcB80;
+    // address myAddress = 0x4a909F94bb4fb71b9E9dA8F3aAc58565310bcB80;
 
-    bytes32 favoriteBytes32 = 'cat'; // can be hex value like  0x2aser25fs or something else
+    // bytes32 favoriteBytes32 = 'cat'; // can be hex value like  0x2aser25fs or something else
 
+    function store(uint256 _favoriteNumber ) public {
+        favoriteNumber = _favoriteNumber ;
+        favoriteNumber = favoriteNumber + 1;
+    }
     
+
+    function retrieve( ) public view returns( uint) {
+        return favoriteNumber;
+    }
+
+
 
 }
