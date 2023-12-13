3cc9823af465dc21ac31a60fa09e9aa4428023c7
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-12-13 18:24:00 +0530

    soliditt types

--- pattrik-collins/simple-storage/demo.sol
+++ pattrik-collins/simple-storage/demo.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0; // solidity versions
 
 contract Demo {
+    bool hasFavoriteNumber = true;
+
+    uint favoriteNumber = 88; // int can be positive or negative , but uint is always positive
+
+    string favoriteNumberText = "88/ eighty-eight";
+
+    address myAddress = 0x4a909F94bb4fb71b9E9dA8F3aAc58565310bcB80;
+
+    bytes32 favoriteBytes32 = 'cat'; // can be hex value like  0x2aser25fs or something else
+
     
+
 }
