f5db384a92e7462decf323a94ac349ed61916dd7
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-12-14 21:15:40 +0530

    structure

--- pattrik-collins/simple-storage/demo.sol
+++ pattrik-collins/simple-storage/demo.sol
@@ -5,6 +5,17 @@ contract Demo {
     // bool hasFavoriteNumber = true;
 
     uint public favoriteNumber = 88; // int can be positive or negative , but uint is always positive
+    
+    // uint[] listOfFavoriteNumbers;  // array declaration
+
+    struct Person {
+        uint favNumber;
+        string name;
+    }  // Structure Declaration
+
+    Person public pat = Person(7, 'Pat'); 
+    Person public mariah = Person({ favNumber: 202, name:'Mariah'}); 
+    Person public jon = Person({ favNumber: 99, name:'Jon'}); 
 
     // string favoriteNumberText = "88/ eighty-eight";
 
