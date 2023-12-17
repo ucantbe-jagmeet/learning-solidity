8d98b0fa65b14fd9fad20a81b57da7e3fd57bc0b
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2023-12-17 15:38:08 +0530

    nested mapping

--- pattrik-collins/simple-storage/demo.sol
+++ pattrik-collins/simple-storage/demo.sol
@@ -1,30 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0; // solidity versions
+pragma solidity ^0.8.20; // solidity versions
 
-contract Demo {
-    // struct Person{
-    //     uint favoriteNumber;
-    //     string name;
-    // }
+contract NestedMapping {
+        // Nested mapping (mapping from address to another mapping)
+    mapping(address => mapping(uint => bool)) public nested;
 
-    // Person[] public listOfPeople;
-
-     mapping(address => uint) public myMap;
-
-    function get(address _addr) public view returns (uint) {
-        // Mapping always returns a value.
-        // If the value was never set, it will return the default value.
-        return myMap[_addr];
+    function get(address _addr1, uint _i) public view returns (bool) {
+        // You can get values from a nested mapping
+        // even when it is not initialized
+        return nested[_addr1][_i];
     }
 
-    function set(address _addr, uint _i) public {
-        // Update the value at this address
-        myMap[_addr] = _i;
+    function set(address _addr1, uint _i, bool _boo) public {
+        nested[_addr1][_i] = _boo;
     }
 
-    function remove(address _addr) public {
-        // Reset the value to the default value.
-        delete myMap[_addr];
+    function remove(address _addr1, uint _i) public {
+        delete nested[_addr1][_i];
     }
 
 }
