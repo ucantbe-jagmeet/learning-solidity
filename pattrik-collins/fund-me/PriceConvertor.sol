0bf803d2ed2ab40a80373594e2ecb2d32750ce1a
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2024-01-04 22:29:04 +0530

    price converter contract added

---
+++ pattrik-collins/fund-me/PriceConvertor.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// Why is this a library and not abstract?
+// Why not an interface?
+library PriceConverter {
+    // We could make this public, but then we'd have to deploy it
+
+}
