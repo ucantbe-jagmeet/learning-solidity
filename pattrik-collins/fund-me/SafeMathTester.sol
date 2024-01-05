9017efbd16042342c633c7be6706d8c16eb42903
ucantbe-jagmeet <jagmeetsingh2921@gmail.com>
2024-01-05 11:54:22 +0530

    safe Math tester unchecked keyword

--- pattrik-collins/fund-me/SafeMathTester.sol
+++ pattrik-collins/fund-me/SafeMathTester.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 contract SafeMathTester { 
-    uint8 public bigNumber = 255; // unchecked in version 0.6.0 but it will check condition in 0.8.0 
-    // and contract will fail it we add 1 to this bigNumber
+    uint8 public bigNumber = 255; // checked
 
     function add() public {
-        bigNumber = bigNumber + 1;
+        unchecked { bigNumber = bigNumber + 1;}
     }
 
 }
